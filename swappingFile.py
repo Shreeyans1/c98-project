e04f8cb9fb29c00fea5e2b9523c5ade27bb8001e
Shreeyans Sharma <64155592+Shreeyans1@users.noreply.github.com>
2021-11-03 18:20:34 +0530

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,17 @@
+def swappingFileData():
+    f1 = input('enter the first file name: ')
+    f2 = input('enter the second file name: ')
+
+    with open(f1, 'r') as a:
+        data_a = a.read()
+
+    with open(f2, 'r') as b:
+        data_b = b.read()
+
+    with open(f1, 'w') as a:
+        a.write(data_b)
+
+    with open(f2, 'w') as b:
+        b.write(data_a)
+
+swappingFileData()
